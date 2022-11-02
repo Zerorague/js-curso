7862b2d7e5f2f910846b97083c758ddafdaa9188
Julio <julioasmb@gmail.com>
2022-11-02 17:54:08 -0300

    objero_console minuto 10

---
+++ Objeto_console.js
@@ -0,0 +1,17 @@
+//objeto console
+
+console.log(console) //es un registro de lo que ha pasado en nuestra aplicacion
+
+console.error("esto es un error") //se pinta en rojo
+
+console.warn("esto es un aviso!") //se pinta en amarillo
+
+console.info("esto es un mensaje informativo")
+
+let nombre = "Julio",
+apellido="san martin",
+edad=29;
+
+console.log(nombre,apellido,edad)
+
+console.log("hola mi nombre es %s %s de %d años de edad",nombre,apellido,edad)
